0c0fb47e20fdf563ea0b1229021f4da1cfb0328f
szhua <0huah0@qq.com>
2015-11-15 16:35:59 +0800

    all run

---
+++ info/tables_c.t
@@ -0,0 +1,75 @@
+create table share(
+	uk varchar(100),
+	shareid varchar(100),
+	feed_type varchar(100),
+	category int,
+	public int,
+	data_id varchar(100),
+	title varchar(1000),
+	third int,
+	clienttype int,
+	filecount int,
+	username varchar(100),
+	feed_time varchar(100),
+	desc_ varchar(1000),
+	avatar_url varchar(1000),
+	category_6_cnt int,
+	source_uid varchar(100),
+	source_id varchar(100),
+	shorturl varchar(100),
+	vcnt int,
+	dcnt int,
+	tcnt int,
+	like_status int,
+	like_count int,
+	comment_count int,
+	create_dt timestamp default now()
+);
+
+create table filelist(
+	shareid varchar(100),	#!!
+	server_filename varchar(1000),
+	category int,
+	isdir int,
+	size int(10),
+	fs_id varchar(100),
+	path varchar(1000),
+	md5 varchar(100),
+	sign varchar(100),
+	time_stamp varchar(100),
+	create_dt timestamp default now()
+);
+
+create table user_info(
+	avatar_url varchar(1000),
+	fans_count int,
+	follow_count int,
+	album_count int,
+	intro varchar(1000),
+	uname varchar(100),
+	uk varchar(100),
+	pubshare_count int,
+	tui_user_count int,
+	c2c_user_sell_count int,
+	c2c_user_buy_count int,
+	c2c_user_product_count int,
+	pair_follow_type int,
+	create_dt timestamp default now()
+);
+
+
+create table fans_list(
+	uk varchar(100),	#!!
+	fans_uk varchar(100),
+	follow_time varchar(100),
+	create_dt timestamp default now()
+);
+
+
+
+create table follow_list(
+	uk varchar(100),	#!!
+	follow_uk varchar(100),
+	follow_time varchar(100),
+	create_dt timestamp default now()
+);
--- src/bdp_crawl.py
+++ src/bdp_crawl.py
@@ -14,29 +14,28 @@ Created on 2015年11月15日
 
 import urllib.request, json, mysql.connector
 
-
-def db_exec(sql, args):
+def db_exec(action,sql,args):
     print("exec:"+sql)
     print(args)
-    
     results=0
+    
     try:
         conn = mysql.connector.connect(host='localhost', user='root', passwd='root', db='bdp', port=3306)
         cur = conn.cursor()
         
-        if args==None:
-            cur.execute(sql, args)
-            results=cur.fetchall()
-            print(results)
-        elif "count"==args:
-            results=cur.execute(sql)
-            if results==None:
-                results=0;
-            print(results)
-        else:
+        if "insert"==action:  # INSERT
             # cur.execute('insert into test values(%s,%s)',value)
             cur.execute(sql, args)
             conn.commit()
+        elif "count"==action: #COUNT
+            results=cur.execute(sql % (args))
+            if results==None:
+                results=0;
+            print(results)
+        else:   # LIST
+            cur.execute(sql % (args))
+            results=cur.fetchall()
+            print(results)
             
         cur.close()
         conn.close()
@@ -53,7 +52,7 @@ def db_exec(sql, args):
 #isExists
 def isExists(uk):
     print("isExists:"+uk)
-    return (db_exec("select count(*) from user_info where uk='"+uk+"'","count")>0) 
+    return (db_exec("count","select count(*) from user_info where uk=%s;",uk)>0) 
 
 
 def fetch_data(url,start):
@@ -68,14 +67,12 @@ def crawl_update_user(uk):
     d=fetch_data("http://yun.baidu.com/pcloud/user/getinfo?query_uk="+uk,0)
     d=d['user_info']
     
-    #db_exec("delete from user_info where uk='"+uk+"'",None) #delete exist
-    
     sql="insert into user_info(avatar_url,fans_count,follow_count,album_count,intro,uname,"
-    sql+="uk,pubshare_count,tui_user_count,c2c_user_sell_count,c2c_user_buy_count,c2c_user_product_count,pair_follow_type)values"
-    sql+="(%s,%s,%s,%s,%s,%s ,%s,%s,%s,%s,%s,%s,%s)"
+    sql+="uk,pubshare_count,tui_user_count,c2c_user_sell_count,c2c_user_buy_count,c2c_user_product_count,pair_follow_type) values "
+    sql+="(%s,%s,%s,%s,%s,%s ,%s,%s,%s,%s,%s,%s,%s);"
     
     d=[d['avatar_url'],d['fans_count'],d['follow_count'],d['album_count'],d['intro'],d['uname'],d['uk'],d['pubshare_count'],d['tui_user_count'],d['c2c_user_sell_count'],d['c2c_user_buy_count'],d['c2c_user_product_count'],d['pair_follow_type']]
-    db_exec(sql,d) #insert new
+    db_exec("insert",sql,d) #insert new
     
 
 
@@ -84,27 +81,39 @@ def crawl_save_share(uk):
     url="http://yun.baidu.com/pcloud/feed/getsharelist?auth_type=1&limit=100&query_uk="+uk
     d=fetch_data(url,0)
     tc=d['total_count']
-    start=10
-    
-    ls=list(d['records'])
+    start=0
     
     sql="insert into share(uk,shareid,feed_type,category,public,data_id,title,third,clienttype,filecount,"
     sql+="username,feed_time,desc_,avatar_url,category_6_cnt,source_uid,source_id,shorturl,vCnt,dCnt,"
-    sql+="tCnt,like_status,like_count,comment_count)values"
-    sql+="(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,"
-    sql+="%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,"
-    sql+="%s,%s,%s,%s"
+    sql+="tCnt,like_status,like_count,comment_count) values "
+    sql+="(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,"
+    sql+=" %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,"
+    sql+=" %s,%s,%s,%s);"
+    
+    
+    fsql="insert into filelist(shareid,server_filename,category,isdir,size,fs_id,path,md5,sign,time_stamp) values "
+    fsql+="(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
     
     while tc>start:     #by page
-        d=fetch_data(url,start)
-        start+=10   #++
         
         ls=list(d['records'])
         
         for d in ls :
-            #d['uk'],d['shareid'],d['feed_type'],d['category'],d['public'],d['data_id'],d['title'],d['third'],d['clienttype'],d['filecount'],d['username'],d['feed_time'],d['desc_'],d['avatar_url'],d['category_6_cnt'],d['source_uid'],d['source_id'],d['shorturl'],d['vCnt'],d['dCnt'],d['tCnt'],d['like_status'],d['like_count'],d['comment_count']
-            db_exec(sql,d) #insert new
-    
+            category_6_cnt=d.get('category_6_cnt',-1)
+            if category_6_cnt==-1:category_6_cnt=d.get('category_4_cnt',-1)
+            
+            db_exec("insert",sql,[d['uk'],d['shareid'],d['feed_type'],d['category'],d['public'],d['data_id'],d['title'],d['third'],d['clienttype'],d['filecount'],d['username'],d['feed_time'],d['desc'],d['avatar_url'],category_6_cnt,d['source_uid'],d['source_id'],d['shorturl'],d['vCnt'],d['dCnt'],d['tCnt'],d['like_status'],d['like_count'],d['comment_count']]) #insert new
+            
+            
+            #save files
+            filelist=d.get('filelist',list([]))
+            
+            for f in filelist :
+                db_exec("insert",fsql,[d['shareid'],f['server_filename'],f['category'],f['isdir'],f['size'],f['fs_id'],f['path'],f['md5'],f['sign'],f['time_stamp']]) #insert new
+        
+        d=fetch_data(url,start)
+        start+=10   #++
+        
     print("Success.total_count=" + str(tc))
     
     
@@ -114,20 +123,20 @@ def crawl_save_follow(uk):
     #max_limit=25
     url="http://yun.baidu.com/pcloud/friend/getfollowlist?limit=10&query_uk="+uk
     d=fetch_data(url,0)
-    ls=list(d['follow_list'])
     tc=d['total_count']
-    start=10
+    start=0
     
-    sql="insert into follow_list(uk,follow_uk,follow_time)values(%s,%s,%s)"
+    sql="insert into follow_list(uk,follow_uk,follow_time) values (%s,%s,%s);"
 
     while tc>start:     #by page
-        d=fetch_data(url,start)
-        start+=10   #++
         
-        ls=list(d['follow_list'])
+        ls=d['follow_list']
         
         for d in ls :
-            db_exec(sql,[uk,d['follow_uk'],d['follow_time']]) #insert new
+            db_exec("insert",sql,[uk,d['follow_uk'],d['follow_time']]) #insert new
+        
+        d=fetch_data(url,start)
+        start+=10   #++
         
     print("crawl_save_follow end.total_count=" + str(tc))
     
@@ -138,28 +147,26 @@ def crawl_save_fans(uk):
     #max_limit=25
     url="http://yun.baidu.com/pcloud/friend/getfanslist?limit=10&query_uk="+uk
     d=fetch_data(url,0)
-    ls=list(d['fans_list'])
     tc=d['total_count']
-    start=10
+    start=0
     
-    sql="insert into fans_list(uk,fans_uk,follow_time)values(%s,%s,%s)"
+    sql="insert into fans_list(uk,fans_uk,follow_time) values (%s,%s,%s);"
 
     while tc>start:     #by page
-        d=fetch_data(url,start)
-        start+=10   #++
         
-        ls=list(d['fans_list'])
+        ls=d['fans_list']
         
         for d in ls :
-            db_exec(sql,[uk,d['fans_uk'],d['follow_time']]) #insert new
+            db_exec("insert",sql,[uk,d['fans_uk'],d['follow_time']]) #insert new
         
-        
+        d=fetch_data(url,start)
+        start+=10   #++
     print("crawl_save_follow end.total_count=" + str(tc))
     
 
 #loop_follow
 def loop_follow(uk):
-    uks=db_exec("select follow_uk from follow_list where uk="+uk,None) #insert new
+    uks=db_exec("list","select follow_uk from follow_list where uk='%s';",uk)
     print(uks)
     if uks!=0:
         for uk in uks:
@@ -167,7 +174,7 @@ def loop_follow(uk):
 
 #loop_fans
 def loop_fans(uk):
-    uks=db_exec("select fans_uk from fans_list where uk="+uk,None) #insert new
+    uks=db_exec("list","select fans_uk from fans_list where uk='%s';",uk) 
     print(uks)
     if uks!=0:
         for uk in uks:
@@ -179,15 +186,20 @@ def do_crawl(uk):
     if isExists(uk):
         print("skip exits uk:"+uk)
     else:
-        crawl_update_user(uk)
+        #crawl_update_user(uk)
         crawl_save_share(uk)
-        crawl_save_follow(uk)
-        crawl_save_fans(uk)
+        #crawl_save_follow(uk)
+        #crawl_save_fans(uk)
         loop_follow(uk)
-        loop_fans(uk)
+        #loop_fans(uk)
         
         
         
 do_crawl("891489109")
 
-
+#    
+# sql="insert into share(uk,shareid,feed_type,category,public,data_id,title,third,clienttype,filecount,username,feed_time,desc_,avatar_url,category_6_cnt,source_uid,source_id,shorturl,vCnt,dCnt,tCnt,like_status,like_count,comment_count) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s,%s,%s,%s);"
+#    
+# args=[490155926, '908522425', 'share', 6, '1', '6062932482101822284', 'K4_4.0.0_to_4.0.1_版本.rar', 0, 0, 1, '没我找不到的书', 1406610874000, '', 'http://himg.bdimg.com/sys/portrait/item/611d4933.jpg', 1, '860429665', '908522425', '1bnneBUJ', 271, 27, 142, 0, 1, 0]
+#    
+# db_exec(sql, args);

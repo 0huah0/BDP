fc478e6b90f0e544d0d5185b0e7e8e10dfbfd604
szhua <0huah0@qq.com>
2015-11-21 18:15:47 +0800

    +1

--- src/bdp_crawl.py
+++ src/bdp_crawl.py
@@ -15,20 +15,23 @@ Created on 2015年11月15日
 import urllib.request, json
 import socket,urllib.parse
 import bdp_db
+from time import sleep
+import threading
 
-logging = bdp_db.logging
+logger = bdp_db.logger
 
 #isExists
 def isExists(uk):
-    logging.warning("isExists:"+uk)
-    return (bdp_db.db_exec("count","select count(*) from user_info where uk=%s;",uk)>0) 
+    logger.debug("isExists:"+uk)
+    if bdp_db.db_exec("count","select count(*) from user_info where uk=%s;",uk)>0 :
+        return 1
+    return 0 
 
 
 def fetch_data(url,start):
     url=url+"&start="+str(start)
-    print("fetching...:"+url);
-    logging.debug("fetching...:"+url)
-        
+    logger.info("fetching...:"+url)
+    
     d=None
     try:
         socket.setdefaulttimeout(10)
@@ -41,31 +44,29 @@ def fetch_data(url,start):
         params = urllib.parse.parse_qs(result.query,True)
         
         if result.path=="/pcloud/feed/getsharelist":    #如果是share则添加req.add_header("Referer",解决start超过500返回errorno=2
-            #print(params['query_uk'][0])
             req.add_header("Referer","http://yun.baidu.com/share/home?uk="+params['query_uk'][0]+"&view=share")
         
         d=urllib.request.urlopen(req).read()
         d=str(d.decode('utf-8'))
         d=json.loads(d)
     except urllib.request.HTTPError as err:
-        logging.warning("Failed.fetching:")
-        logging.warning(err)
+        logger.error("Failed.fetching:",err)
     except BaseException as err:
-        logging.warning(err)
+        logger.error(err)
     
     if d==None:d={}
     return d #反序列化
 
-#crawl_update_user
-def crawl_update_user(uk):
+#crawl_save_user
+def crawl_save_user(uk):
     d=fetch_data("http://yun.baidu.com/pcloud/user/getinfo?query_uk="+uk,0)
     d=d.get('user_info',None)
     if d!=None:
         try:
-            d=[d['avatar_url'],d['fans_count'],d['follow_count'],d['album_count'],d['intro'],d['uname'],d['uk'],d['pubshare_count'],d['tui_user_count'],d['c2c_user_sell_count'],d['c2c_user_buy_count'],d['c2c_user_product_count'],d['pair_follow_type']]
+            d=[d.get('avatar_url',''),d.get('fans_count',''),d.get('follow_count',''),d.get('album_count',''),d.get('intro',''),d.get('uname',''),d.get('uk',''),d.get('pubshare_count',''),d.get('tui_user_count',''),d.get('c2c_user_sell_count',''),d.get('c2c_user_buy_count',''),d.get('c2c_user_product_count',''),d.get('pair_follow_type','')]
             bdp_db.db_exec("insert",bdp_db.insert_user,d) #insert new
         except Exception as e:
-            logging.warn("skip insert.err:"+str(e))
+            logger.error("skip insert.err:"+str(e))
 
 
 def crawl_save_share(uk):
@@ -86,24 +87,25 @@ def crawl_save_share(uk):
             if category_6_cnt==-1:category_6_cnt=d.get('category_4_cnt',-1)
             
             try:
-                bdp_db.db_exec("insert",bdp_db.insert_share,[d['uk'],d['shareid'],d['feed_type'],d['category'],d['public'],d['data_id'],d['title'],d['third'],d['clienttype'],d['filecount'],d['username'],d['feed_time'],d['desc'],d['avatar_url'],category_6_cnt,d['source_uid'],d['source_id'],d['shorturl'],d['vCnt'],d['dCnt'],d['tCnt'],d['like_status'],d['like_count'],d['comment_count']]) #insert new
+                bdp_db.db_exec("insert",bdp_db.insert_share,[d.get('uk',''),d.get('shareid',''),d.get('feed_type',''),d.get('category',''),d.get('public',''),d.get('data_id',''),d.get('title',''),d.get('third',''),d.get('clienttype','')\
+                                                             ,d.get('filecount',''),d.get('username',''),d.get('feed_time',''),d.get('desc',''),d.get('avatar_url',''),category_6_cnt,d.get('source_uid',''),d.get('source_id',''),d.get('shorturl',''),d.get('vCnt',''),d.get('dCnt',''),d.get('tCnt',''),d.get('like_status',''),d.get('like_count',''),d.get('comment_count','')]) #insert new
             except Exception as e:
-                logging.warn("skip insert.err:"+str(e))
+                logger.warn("skip insert.err:"+str(e))
             
             #save files
             filelist=d.get('filelist',[])
             
             for f in filelist :
                 try:
-                    bdp_db.db_exec("insert",bdp_db.insert_file,[d['shareid'],f['server_filename'],f['category'],f['isdir'],f['size'],f['fs_id'],f['path'],f['md5'],f['sign'],f['time_stamp']]) #insert new
+                    bdp_db.db_exec("insert",bdp_db.insert_file,[d.get('shareid',''),f.get('server_filename',''),f.get('category',''),\
+                                                                f.get('isdir',''),f.get('size',''),f.get('fs_id',''),f.get('path',''),f.get('md5',''),f.get('sign',''),f.get('time_stamp','')]) #insert new
                 except Exception as e:
-                    logging.warn("skip insert.err:"+str(e))
+                    logger.error("skip insert.err:"+str(e))
                     
         d=fetch_data(url,start)
         start+=max_limit   #++
         
-    print("Success.total_count=" + str(tc))
-    
+    logger.debug("Success.total_count=" + str(tc))
     
     
 #crawl_save_follow
@@ -121,14 +123,15 @@ def crawl_save_follow(uk):
         
         for d in ls :
             try:
-                bdp_db.db_exec("insert",bdp_db.insert_follow,[uk,d['follow_uk'],d['follow_time']]) #insert new
+                bdp_db.db_exec("insert",bdp_db.insert_follow,[uk,d.get('follow_uk',''),d.get('follow_time','')]) #insert new
+                bdp_db.db_exec("insert","insert into bdp_new_uk(uk)values(%s);",[d.get('follow_uk','')]) #insert new
             except Exception as e:
-                logging.warn("skip insert.err:"+str(e))
+                logger.error("skip insert.err:"+str(e))
         
         d=fetch_data(url,start)
         start+=max_limit   #++
         
-    print("crawl_save_follow end.total_count=" + str(tc))
+    logger.debug("crawl_save_follow end.total_count=" + str(tc))
     
     
     
@@ -145,49 +148,90 @@ def crawl_save_fans(uk):
         ls=d.get('fans_list',[])
         for d in ls :
             try:
-                bdp_db.db_exec("insert",bdp_db.insert_fans,[uk,d['fans_uk'],d['follow_time']]) #insert new
+                bdp_db.db_exec("insert",bdp_db.insert_fans,[uk,d.get('fans_uk',''),d.get('follow_time','')]) #insert new
+                bdp_db.db_exec("insert","insert into bdp_new_uk(uk)values(%s);",[d.get('fans_uk','')]) #insert new
             except Exception as e:
-                logging.warn("skip insert.err:"+str(e))
-        
+                logger.error("skip insert.err:"+str(e))
         
         d=fetch_data(url,start)
         start+=max_limit   #++
-    print("crawl_save_follow end.total_count=" + str(tc))
-    
+        
+    logger.info("crawl_save_follow end.total_count=" + str(tc))
+
 
 #loop_follow
 def loop_follow(uk):
     uks=bdp_db.db_exec("list","select follow_uk from follow_list where uk='%s';",uk)
-    print(uks)
+    logger.debug(uks)
     if uks!=0:
         for uk in uks:
-            do_crawl(uk[0])
+            do_crawl_rel(uk[0])
+
 
 #loop_fans
 def loop_fans(uk):
     uks=bdp_db.db_exec("list","select fans_uk from fans_list where uk='%s';",uk) 
-    print(uks)
+    logger.debug(uks)
     if uks!=0:
         for uk in uks:
-            do_crawl(uk[0])
+            do_crawl_rel(uk[0])
     
     
     
-def do_crawl(uk):
+#爬取关系图    
+def do_crawl_rel(uk):
     if isExists(uk):
-        print("skip exits uk:"+uk)
+        logger.warn("skip exits uk:"+uk)
     else:
-        crawl_update_user(uk)
-        crawl_save_share(uk)
         crawl_save_follow(uk)
         crawl_save_fans(uk)
         loop_follow(uk)
         loop_fans(uk)
         
         
+#爬取人员信息和文件信息        
+miss_times=0
+def do_crawl():
+    while 1:
+        uks=bdp_db.db_exec("list","select uk from bdp_new_uk limit %s;","20") ;
+        if(len(uks)>0): 
+            for uk in uks:
+                uk=uk[0]
+                if isExists(uk):
+                    logger.warn("skip exits uk:"+uk)
+                else:
+                    crawl_save_share(uk)
+                    crawl_save_user(uk)
+                    bdp_db.db_exec("delete","delete from bdp_new_uk where uk="+str(uk),uk) ;
+        else:
+            global miss_times
+            miss_times+=1
+            if(miss_times>10):
+                break
+            sleep(2)
+        
+     
+     
         
 #do_crawl("891489109")
-do_crawl("490155926")
+#do_crawl("490155926")
+
+
+uk=bdp_db.db_exec("list","select uk from bdp_new_uk limit %s;","1")[0][0]
+# 创建两个线程
+threads = []
+t1 = threading.Thread(target=do_crawl_rel,args=(str(uk),))
+threads.append(t1)
+
+bdp_db.db_exec("insert","insert into bdp_new_uk(uk)values(%s);",[uk]) #insert new
+t2 = threading.Thread(target=do_crawl,args=())
+threads.append(t2)
+
+for t in threads:
+    t.setDaemon(True)
+    t.start()
+
+t.join()
 
 #    
 # sql="insert into share(uk,shareid,feed_type,category,public,data_id,title,third,clienttype,filecount,username,feed_time,desc_,avatar_url,category_6_cnt,source_uid,source_id,shorturl,vCnt,dCnt,tCnt,like_status,like_count,comment_count) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s,%s,%s,%s);"
